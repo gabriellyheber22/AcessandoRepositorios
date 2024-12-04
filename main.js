ae2948c6c408128510848426c1a068238eb90462
gabriellyheber22 <gabriellyheber@gmail.com>
2024-12-04 11:03:21 -0300

    Aplicação de estilos
    Organização de elemento
    Aplicação de efeito no botão

--- main.js
+++ main.js
@@ -1,16 +1,22 @@
 
 function repos() {
     let usuario = document.getElementById('user').value;
+    let input = document.getElementById('user');
+    let retornoGit = document.getElementById('retorno-git');
+    let divLista = document.getElementById('lista-repositorio');
     let listaRepositirios = document.getElementById('lista');
+    let tituloLista = document.getElementById('titulo-lista')
     let statusCarregando = document.getElementById('carregando');
     listaRepositirios.innerText = '';
+    input.value = ''
     statusCarregando.style.display = 'block';
     axios.get('https://api.github.com/users/' + usuario + '/repos')
         .then(function (response) {
+            retornoGit.style.display ='flex'
             statusCarregando.style.display = 'none';
             //O forEach() percorre cada item do array e executa a função fornecida para cada elemento. Nesse caso, 'repositorio' é o item atual da iteração
             response.data.forEach((repositorio) => {
-                
+               
                 item = document.createElement('li');//Criando o item
                 listaRepositirios.appendChild(item);//Adicionando o item a lista
                 let itemTextName = document.createTextNode(repositorio.name); //pegando o retorno do repositorio e criando um text node
@@ -23,10 +29,18 @@ function repos() {
             });
         })
         .catch(function (error) {
+            tituloLista.style.display ='none';
+            retornoGit.style.display ='flex';
+            retornoGit.style.alignItems = 'center'
+            divLista.style.display ='flex';
+            divLista.style.justifyContent = 'center';
+            divLista.style.alignItems = 'center'
+            divLista.style.flexDirection = 'column';
             statusCarregando.style.display = 'none';
             item = document.createElement('li');//Criando o item
                 listaRepositirios.appendChild(item);
                 let itemTextError = document.createTextNode('Código do erro 404');
+               
                 item.appendChild(itemTextError);
         })
 }
